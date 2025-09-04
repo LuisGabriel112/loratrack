40446a86dd5847c22f410a7b68dc7724c2705b67
Luis Gabriel Venegas Saucedo <venegassaucedoluis@gmail.com>
2025-09-04 13:24:57 -0600

    cambio de aspecto

--- loratrack/lorafi/admin.py
+++ loratrack/lorafi/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import dispositivos
-from .models import nodo
-# Register your models here.
-admin.site.register(dispositivos)
-admin.site.register(nodo)
+from .models import dispositivos, nodo
+
+class NodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('latitud', 'longitud', 'ultima_vez_visto')
+
+class DispositivosAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(dispositivos, DispositivosAdmin)
+admin.site.register(nodo, NodoAdmin)
